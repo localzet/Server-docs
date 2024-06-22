6046850341113a20fc4e78a7cdef151525ea501f
Ivan Zorin <creator@localzet.com>
2024-06-22 15:35:53 +0300

    Update

--- src/components/HeroPattern.jsx
+++ src/components/HeroPattern.jsx
@@ -3,19 +3,21 @@ export function HeroPattern() {
         <div className="absolute inset-0 -z-10 mx-0 max-w-none flex justify-center overflow-hidden">
             <div className="w-[108rem] flex-none flex justify-end">
                 <picture>
-                    <source srcSet="/images/docs@30.8b9a76a2.avif" type="image/avif"/>
+                    <source srcSet="/images/docs@30.8b9a76a2.avif" type="image/avif" className="not-prose"/>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                         src="/images/docs@tinypng.d9e4dcdc.png" alt=""
-                        className="w-[71.75rem] flex-none max-w-none dark:hidden" decoding="async"/>
+                        className="not-prose w-[71.75rem] flex-none max-w-none dark:hidden" decoding="async"/>
                 </picture>
                 <picture>
-                    <source srcSet="/images/docs-dark@30.1a9f8cbf.avif" type="image/avif"/>
+                    <source srcSet="/images/docs-dark@30.1a9f8cbf.avif" type="image/avif" className="not-prose"/>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img src="/images/docs-dark@tinypng.1bbe175e.png" alt=""
-                         className="w-[90rem] flex-none max-w-none hidden dark:block" decoding="async"/>
+                         className="not-prose w-[90rem] flex-none max-w-none hidden dark:block" decoding="async"/>
                 </picture>
             </div>
         </div>
+
+
     )
 }
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -22,13 +22,67 @@ export default function App({Component, pageProps}) {
     return (
         <>
             <Head>
-                {router.pathname === '/' ? (
+                {router.pathname === '/' && pageProps.title ? (
                     <title>Localzet Server</title>
                 ) : (
                     <title>{`${pageProps.title} - Localzet Server`}</title>
                 )}
-                <meta name="description" content={pageProps.description}/>
-                <link rel="icon" href="/server.png" />
+                <meta charSet="utf-8"/>
+
+                <meta name="title"
+                      content={pageProps.title ? `${pageProps.title} - Localzet Server` : 'Localzet Server'}/>
+                <meta name="description"
+                      content={pageProps.description ?? 'Localzet Server is an asynchronous event-based server in PHP, offering high performance and scalability using modern technologies and standards'}/>
+                <meta name="keywords" content={pageProps.keywords}/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+                <meta name="og:title"
+                      content={pageProps.title ? `${pageProps.title} - Localzet Server` : 'Localzet Server'}/>
+                <meta name="og:description"
+                      content={pageProps.description ?? 'Localzet Server is an asynchronous event-based server in PHP, offering high performance and scalability using modern technologies and standards.'}/>
+                <meta name="og:image"
+                      content={`https://cover.pr-cy.io/api/og?logo=https%3A%2F%2Fdocs.localzet.com%2Fserver.png&bgColor=0f172a&color=ffffff&title=${encodeURIComponent(pageProps.title ?? 'Localzet Server')}&category=${encodeURIComponent('Доументация - Localzet Server')}`}/>
+                <meta name="og:url" content={`https://docs.localzet.com${router.asPath}`}/>
+                <meta name="og:type" content="website"/>
+                <meta name="og:locale" content="ru_RU"/>
+                <meta name="og:site_name" content="Localzet Server"/>
+
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content={`https://docs.localzet.com${router.asPath}`}/>
+                <meta property="twitter:title"
+                      content={pageProps.title ? `${pageProps.title} - Localzet Server` : 'Localzet Server'}/>
+                <meta property="twitter:description"
+                      content={pageProps.description ?? 'Localzet Server is an asynchronous event-based server in PHP, offering high performance and scalability using modern technologies and standards.'}/>
+                <meta name="twitter:image"
+                      content={`https://cover.pr-cy.io/api/og?logo=https%3A%2F%2Fdocs.localzet.com%2Fserver.png&bgColor=0f172a&color=ffffff&title=${encodeURIComponent(pageProps.title ?? 'Localzet Server')}&category=${encodeURIComponent('Доументация - Localzet Server')}`}/>
+
+                <meta name="language" content="RU"/>
+                <meta name="robots" content="index,follow"/>
+
+                <meta name="apple-mobile-web-app-capable" content="yes"/>
+                <meta name="apple-touch-fullscreen" content="yes"/>
+                <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+                <meta name="format-detection" content="telephone=no"/>
+
+                <meta name="copyright" content="Localzet Group"/>
+                <meta name="reply-to" content="support@localzet.com"/>
+                <meta name="owner" content="Ivan Zorin <creator@localzet.com>"/>
+                <meta name="author" content="Ivan Zorin <creator@localzet.com>"/>
+                <meta name="creator" content="Ivan Zorin <creator@localzet.com>"/>
+                <meta name="designer" content="Ivan Zorin <creator@localzet.com>"/>
+
+                <meta name="application-name" content="Localzet Server"/>
+                <meta name="subject"
+                      content="Localzet Server is an asynchronous event-based server in PHP, offering high performance and scalability using modern technologies and standards"/>
+                <meta name="generator" content="Triangle Framework"/>
+
+                <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0f172a"/>
+                <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff"/>
+
+                <link rel="icon" type="image/png" href="/server.png"/>
+                <link rel="canonical" href={`https://docs.localzet.com${router.asPath}`}/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             </Head>
             <MDXProvider components={mdxComponents}>
                 <Layout {...pageProps}>
