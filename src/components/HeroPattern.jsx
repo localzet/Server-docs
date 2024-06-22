0926e231a5769ef3ff895bef2d5214c6a2ef12ca
Ivan Zorin <creator@localzet.com>
2024-06-22 03:58:50 +0300

    Update

--- src/components/HeroPattern.jsx
+++ src/components/HeroPattern.jsx
@@ -17,7 +17,5 @@ export function HeroPattern() {
                 </picture>
             </div>
         </div>
-
-
     )
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,10 @@
+import Image from 'next/image'
+
 export function Logo() {
     return (
         <div className="h-10">
-            <img src="/server-full-dark.png" className="block dark:hidden h-10" alt="Logo"/>
-            <img src="/server-full-light.png" className="hidden dark:block h-10" alt="Logo"/>
+            <Image src="/server-full-dark.png" className="block dark:hidden h-10" alt="Logo"/>
+            <Image src="/server-full-light.png" className="hidden dark:block h-10" alt="Logo"/>
         </div>
     )
 }
