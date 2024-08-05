3169d429b40eb3704c455bb1a2a2180c7ffa3a54
Ivan Zorin <creator@localzet.com>
2024-08-05 13:17:17 +0500

    Upd

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
     experimental: {
